b2468cb77b809860fd8776b594a549886aee21e7
Minhaj Sadik <MinhajoSadik@gmail.comm>
2021-07-28 04:32:18 +0600

    [code\add] practice code

--- script.js
+++ script.js
@@ -4,20 +4,20 @@
 
 // // check function scope
 
-// function greeting(hello){
-//     var name = 'minhajsadik'
-//     console.log(name);
-// }
-// console.log(name);
-// greeting();
+function greeting(hello){
+    var name = 'minhajsadik'
+    console.log(name);
+}
+console.log(name);
+greeting();
 
-// // global scope
+// global scope
 
-// if(true){
-//     var name = 'minhajsadik';
-//     // console.log(name);
-// }
-// console.log(name);
+if(true){
+    var name = 'minhajsadik';
+    // console.log(name);
+}
+console.log(name);
 
 
 /* 
@@ -36,27 +36,27 @@
 
 
 // Assignment operator
-// let x = 10;
-// let y;
-// y = x;
-// console.log(y);//10
+let x = 10;
+let y;
+y = x;
+console.log(y);//10
 
-// let a = 20;
-// let b = 30;
-// a = b;
-// console.log(a);//30
+let a = 20;
+let b = 30;
+a = b;
+console.log(a);//30
 
 
 // Compound Operators
 
-// let x = 10;
-// let y = 20;
+let x = 10;
+let y = 20;
 
-// x += y; //x = x + y;
-// x -= y; //x = x - y;
-// x *= y; //x = x * y;
-// x /= y; //x = x / y;
-// console.log(x)
+x += y; //x = x + y;
+x -= y; //x = x - y;
+x *= y; //x = x * y;
+x /= y; //x = x / y;
+console.log(x)
 
 // Arithmetic Operators
 /*
@@ -68,11 +68,11 @@
 --
 */
 
-// let x = 10;
-// x++;
-// x--;
+let x = 10;
+x++;
+x--;
 
-// console.log(x ** 2)
+console.log(x ** 2)
 
 
 // Logical Operators -- returns Boolean
@@ -102,51 +102,51 @@ let happy = true && true;
 8 Less than or equal (<=)
 */
 
-// console.log(4 == 4);
-// console.log(4 === 4);
-// console.log(4 != '4');
-// console.log(100 <= 100)
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != '4');
+console.log(100 <= 100)
 
 
 
 
 
 
-// let text = "";
-// const fruits = ["Minhaj", "Ahmed", "Sadik"];
-// fruits.forEach(myFunction);
+let text = "";
+const fruits = ["Minhaj", "Ahmed", "Sadik"];
+fruits.forEach(myFunction);
 
-// document.getElementById("demo").innerHTML = text;
-
-
-// function myFunction(item, index) {
-//   text += index + " : " + item + "<br>";
-// }
+document.getElementById("demo").innerHTML = text;
 
 
-// console.log(6 > 5)
+function myFunction(item, index) {
+  text += index + " : " + item + "<br>";
+}
+
+
+console.log(6 > 5)
 
 
 
-// function square(number){
-//     return number * number;
-// }
+function square(number){
+    return number * number;
+}
 
-// const result = square(7);
-// console.log(result)
+const result = square(7);
+console.log(result)
 
-// function myFunc(theObject) {
-//     theObject.make = 'Toyota';
-//   }
+function myFunc(theObject) {
+    theObject.make = 'Toyota';
+  }
   
-//   var mycar = {make: 'Honda', model: 'Accord', year: 1998};
-//   var x, y;
+  var mycar = {make: 'Honda', model: 'Accord', year: 1998};
+  var x, y;
   
-//   x = mycar.make; // x gets the value "Honda"
+  x = mycar.make; // x gets the value "Honda"
   
-//   myFunc(mycar);
-//   y = mycar.make; // y gets the value "Toyota"
-// // (the make property was changed by the function)
+  myFunc(mycar);
+  y = mycar.make; // y gets the value "Toyota"
+// (the make property was changed by the function)
 
 
 
