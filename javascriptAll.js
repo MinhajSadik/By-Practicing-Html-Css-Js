3d8e86fc70d94cbec1a059b5cc7bd9fb7148c31c
Minhaj Sadik <MinhajoSadik@gmail.comm>
2021-08-02 00:35:18 +0600

    [start] working on aug-2

--- javascriptAll.js
+++ javascriptAll.js
@@ -1,7 +1,35 @@
 //javascript all you need to know//
 //practice in javascript//
 
+console.log('minhaj ' + 'ahmed' + ' age is ' + 24)
+
 var name = 'minhaj';
 var age = 24;
 
 console.log(name, age);
+
+
+// datatypes
+
+// premitive datatypes
+Number
+String
+Boolean
+null
+undefined
+
+// object
+Array
+Object
+Function
+
+var n = 1431
+var f = 3.24
+var nn = Number('34.23')
+// console.log(nn)
+
+console.log(Number.parseInt(nn))
+
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
