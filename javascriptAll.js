bdd95b29e05c4a116235349c1b23b0100690dc81
Minhaj Sadik <MinhajoSadik@gmail.comm>
2021-08-02 00:40:34 +0600

    [start] working on aug-2

--- javascriptAll.js
+++ javascriptAll.js
@@ -17,12 +17,16 @@ String
 Boolean
 null
 undefined
+Infinity //1/0
+NaN //'abc' * 10
+
 
 // object
 Array
 Object
 Function
 
+//**Number**/
 var n = 1431
 var f = 3.24
 var nn = Number('34.23')
@@ -33,3 +37,21 @@ console.log(Number.parseInt(nn))
 console.log(Number.MAX_SAFE_INTEGER)
 console.log(Number.MAX_VALUE)
 console.log(Number.MIN_SAFE_INTEGER)
+
+
+//**String**/
+
+// first 3 which is direct value is set it's called string literal
+const str = 'String';
+const str1 = "String"
+const str2 = `String`
+
+
+const str3 = String('amaar')
+const str4 = String(232)
+const str5 = String(3.23)
+
+
+
+
+
