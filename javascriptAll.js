09f13363fe2be3531779c26ab0f24e412a27ee11
MinhajSadik <MinhajoSadik@gmail.com>
2021-08-06 01:06:49 +0600

    [push] practicing code

---
+++ javascriptAll.js
@@ -0,0 +1,14 @@
+// Arithmetic Operator
+/* '+' '-' '*' '/' '%' '++' '--' */
+
+var a = 11;
+var b = 6 
+
+console.log(a % b);
+
+//Incremental & Decremental
+//Pre Incremental - Post Incremental
+
+console.log(a++)
+console.log(++b);
+
