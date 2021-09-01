aeaf7ce444ab3e65178c07e63ee61d996ac77042
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-02 00:50:56 +0600

    [adding] practice code

---
+++ es6.js
@@ -0,0 +1,16 @@
+// var numbers = [3, 5, 7, 12, 10, 11, 19]
+
+// var result = numbers.splice(1, 3, 5, 7)
+
+
+// var result = numbers.push(20, 30, 40, 50,)
+
+// console.log(result);
+// console.log(numbers);
+
+var numbers = [3, 5, 7, 12, 10, 11, 19]
+
+var result = numbers.push(20, 30, 40, 50, 50, 50 , 10, 11, 19, )
+
+console.log(result);
+console.log(numbers);
