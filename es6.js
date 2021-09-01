a66e11e227cc0591ca42ec72fd13231d47c22fcf
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-02 03:05:00 +0600

    [add] practice code

--- es6.js
+++ es6.js
@@ -8,9 +8,19 @@
 // console.log(result);
 // console.log(numbers);
 
-var numbers = [3, 5, 7, 12, 10, 11, 19]
+// var numbers = [3, 5, 7, 12, 10, 11, 19]
 
-var result = numbers.push(20, 30, 40, 50, 50, 50 , 10, 11, 19, )
+// var result = numbers.push(20, 30, 40, 50, 50, 50 , 10, 11, 19, )
 
-console.log(result);
-console.log(numbers);
+// console.log(result);
+// console.log(numbers);
+
+
+// let number = [20]
+// number = [30, 40, 50, 50]
+// console.log(number);
+
+for (let i = 0; i <= 5; i++){
+    console.log(i);
+}
+// console.log(i);
