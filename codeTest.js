874888b80e1fa8055a1947e97d34b84ce516e2db
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-08 14:32:56 +0600

    [add] a new file

---
+++ codeTest.js
@@ -0,0 +1,13 @@
+// function sayHi(){
+//     console.log(name);
+//     console.log(age);
+//     var name = 'javascript';
+//     var age = 21;
+// }
+// sayHi();
+
+console.log(name);
+console.log(age);
+var name = 'javascript';
+let age = 21;
+
