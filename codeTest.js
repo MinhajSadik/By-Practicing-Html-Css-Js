8153f6cddacf3d909340325e5a0902f55b43c49e
MinhajSadik <MinhajoSadik@gmail.com>
2021-09-09 21:15:39 +0600

    [ADD] testing code

--- codeTest.js
+++ codeTest.js
@@ -1,13 +1,13 @@
-// function sayHi(){
-//     console.log(name);
-//     console.log(age);
-//     var name = 'javascript';
-//     var age = 21;
-// }
-// sayHi();
+function sayHi(){
+    console.log(name);
+    console.log(age);
+    var name = 'javascript';
+    var age = 21;
+}
+sayHi();
 
-console.log(name);
-console.log(age);
-var name = 'javascript';
-let age = 21;
+// console.log(name);
+// console.log(age);
+// var name = 'javascript';
+// let age = 21;
 
